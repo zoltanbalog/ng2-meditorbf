f2f5f4bfb3d7dc0a83521c9a686aa8c332624b59
Balog Zoli <zoltan.balog@appo.io>
2017-01-12 16:21:15 +0100

    add css and eventlisteners

--- medium-editor.component.ts
+++ medium-editor.component.ts
@@ -1,8 +1,6 @@
-import { Component, Input, forwardRef, ElementRef, ViewChild, OnChanges, OnInit, OnDestroy } from '@angular/core';
+import { Component, Input, forwardRef, ElementRef, ViewChild, OnChanges, OnInit, OnDestroy, ViewEncapsulation, Output, EventEmitter } from '@angular/core';
 import * as MediumEditor from './src/MediumEditor/js/medium-editor';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
-
-// const MediumEditor = require('./src/MediumEditor/js/medium-editor.js');
 
 @Component({
     selector: 'medium-editor',
@@ -11,6 +9,7 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
         useExisting: forwardRef(() => MediumEditorComponent),
         multi: true
     }],
+    encapsulation: ViewEncapsulation.None,
     template: `<div #host></div>`,
     styleUrls: [
         './src/MediumEditor/css/font-awesome.min.css',
@@ -25,6 +24,11 @@ export class MediumEditorComponent implements ControlValueAccessor, OnInit, OnDe
     @ViewChild('host') host: any;
     propagateChange = (_: any) => { };
 
+    // Triggering events
+    @Output() focusEvent = new EventEmitter<any>();
+    @Output() keyUpEvent = new EventEmitter<any>();
+    @Output() clickEvent = new EventEmitter<any>();
+
     constructor(el: ElementRef) {
         this.el = el;
     }
@@ -38,10 +42,8 @@ export class MediumEditorComponent implements ControlValueAccessor, OnInit, OnDe
             });
         }
         this.editor = new MediumEditor(this.host.nativeElement, this.options);
-        this.editor.subscribe('editableInput', (event: any, editable: any) => {
-            let value = this.editor.elements[0].innerHTML;
-            this.ngOnChanges(value);
-        });
+
+        this.registerEventListeners();
     }
 
     ngOnDestroy() {
@@ -52,6 +54,22 @@ export class MediumEditorComponent implements ControlValueAccessor, OnInit, OnDe
 
     ngOnChanges(changes: any) {
         this.propagateChange(changes);
+    }
+
+    registerEventListeners() {
+        this.editor.subscribe('editableInput', (event: any, editable: any) => {
+            let value = this.editor.elements[0].innerHTML;
+            this.ngOnChanges(value);
+        });
+        this.editor.subscribe('focus', (data: any, element: any) => {
+            this.focusEvent.emit({'event': data, 'target': element});
+        });
+        this.editor.subscribe('editableKeyup', (data: any, element: any) => {
+            this.keyUpEvent.emit({'event': data, 'target': element});
+        });
+        this.editor.subscribe('editableClick', (data: any, element: any) => {
+            this.clickEvent.emit({'event': data, 'target': element});
+        });
     }
 
     writeValue(value: any) {
